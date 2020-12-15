668540521f9dd051c018d06f93aeb162c84b3a3c
euppal <67026187+euppal@users.noreply.github.com>
2020-12-15 16:46:59 -0500

    Update tests

--- file/main.c
+++ file/main.c
@@ -9,13 +9,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include "file.h"
+#define TRIALS 5
+#define SIZE 0x100000ULL
 
 long test_file() {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     file_t* file = openf("test1", O_CREAT | O_RDWR, 0666, FILE_512_BYTE_BUFFER);
     char str[1] = { 'a' };
-    for (size_t i = 0; i < 8192; i++) {
+    for (size_t i = 0; i < SIZE; i++) {
         writef(file, str, 1);
     }
     closef(file);
@@ -27,7 +29,7 @@ long test_stdio() {
     clock_gettime(CLOCK_MONOTONIC, &start);
     FILE* file = fopen("test2", "w");
     char str[1] = { 'a' };
-    for (size_t i = 0; i < 8192; i++) {
+    for (size_t i = 0; i < SIZE; i++) {
         fwrite(str, sizeof(char), 1, file);
     }
     fclose(file);
@@ -36,13 +38,20 @@ long test_stdio() {
 }
 
 int main(int argc, const char * argv[]) {
-    printf("5 TRIALS\n"
-           "--------\n");
-    for (size_t i = 1; i <= 5; i++) {
-        printf("Trial %zu:\n", i);
-        printf("  file.h: %ld ms\n", test_file() / 100000);
-        printf("  stdio.h: %ld ms\n", test_stdio() / 100000);
+    printf("%d TRIALS\n"
+           "--------\n", TRIALS);
+    long total_file = 0;
+    long total_stdio = 0;
+    for (int i = 1; i <= TRIALS; i++) {
+        const long file_time = test_file() / 100000;
+        total_file += file_time;
+        printf("Trial %d:\n", i);
+        printf("  file.h: %ld ms\n", file_time);
+        const long stdio_time = test_stdio() / 100000;
+        total_stdio += stdio_time;
+        printf("  stdio.h: %ld ms\n", stdio_time);
         system("rm test1; rm test2");
     }
+    printf("AVG file.h: %ld ms, AVG stdio.h: %ld ms\n", total_file / TRIALS, total_stdio / TRIALS);
     return 0;
 }
